c56a001dc6236d5b384b79c09775f57b4ece7aaf
Peter Knall <peterknall@gmail.com>
2021-08-07 11:46:12 -0400

    Initial commit

---
+++ src/main/java/com/github/pknall/jobqueue/JobQueueApplication.java
@@ -0,0 +1,13 @@
+package com.github.pknall.jobqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobQueueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobQueueApplication.class, args);
+    }
+
+}
